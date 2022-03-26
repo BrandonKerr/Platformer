8879ad14a6b6c5026161b8b586bd7d535529ebab
Brandon Kerr <brandon.js.kerr@gmail.com>
2022-03-26 16:50:53 -0400

    Reset score and deaths when playing new run. Only reset the level's score on death

--- src/Autoload/PlayerData.gd
+++ src/Autoload/PlayerData.gd
@@ -3,17 +3,26 @@ extends Node
 signal score_updated
 signal player_died
 
-# TODO level_score so that we add it to score when completing the level and reset to 0 on death or retry
 var score: = 0 setget set_score
+var total_score: = 0 setget set_total_score
 var deaths: = 0 setget set_deaths
 
 func reset() -> void:
 	score = 0
+	total_score = 0
 	deaths = 0
+
+func addLevelScore() -> void:
+	total_score += score
+	score = 0
 
 func set_score(value: int) -> void:
 	score = value
 	emit_signal("score_updated")
+	
+func set_total_score(value: int) -> void:
+	total_score = value
+	emit_signal("total_score_updated")
 
 func set_deaths(value: int) -> void:
 	deaths = value
--- src/Screens/MainScreen.gd
+++ src/Screens/MainScreen.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _ready() -> void:
 	$Menu/PlayButton.grab_focus()
+	# reset the player's score and death in case they're retrying
+	PlayerData.reset()
+	
