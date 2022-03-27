31814d37b734b39f990325a35c2c3868a493f5e4
Brandon Kerr <brandon.js.kerr@gmail.com>
2022-03-27 11:24:55 -0400

    Add link to credits and play music on main screen

--- src/Screens/MainScreen.gd
+++ src/Screens/MainScreen.gd
@@ -4,4 +4,6 @@ func _ready() -> void:
 	$Menu/PlayButton.grab_focus()
 	# reset the player's score and death in case they're retrying
 	PlayerData.reset()
+	# start playing the music
+	MusicController.play_main_music()
 	
